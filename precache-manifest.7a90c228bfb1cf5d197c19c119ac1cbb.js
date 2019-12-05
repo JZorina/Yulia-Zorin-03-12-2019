28fbaefbb2c2db0b83a370d4cb9b5a11d277f560
yulia <yuliazo@ac.sce.ac.il>
2019-12-05 04:08:01 +0200

    Updates

---
+++ precache-manifest.7a90c228bfb1cf5d197c19c119ac1cbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c40b189ab120643fc1bf636fc0942ec6",
+    "url": "/JZorina/Yulia-Zorin-03-12-2019/index.html"
+  },
+  {
+    "revision": "bd01c0f6c0788e23c104",
+    "url": "/JZorina/Yulia-Zorin-03-12-2019/static/css/main.bb48ce6d.chunk.css"
+  },
+  {
+    "revision": "3861361fad97e2a77a44",
+    "url": "/JZorina/Yulia-Zorin-03-12-2019/static/js/2.ed0f5b4f.chunk.js"
+  },
+  {
+    "revision": "bd01c0f6c0788e23c104",
+    "url": "/JZorina/Yulia-Zorin-03-12-2019/static/js/main.0f8800a9.chunk.js"
+  },
+  {
+    "revision": "21abe33f8e605e2c2e69",
+    "url": "/JZorina/Yulia-Zorin-03-12-2019/static/js/runtime-main.543d4251.js"
+  }
+]);
