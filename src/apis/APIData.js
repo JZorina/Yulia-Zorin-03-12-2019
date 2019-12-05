4996d4dc865c3c8abe9edd70d4045b084d7b991a
yulia <yuliazo@ac.sce.ac.il>
2019-12-05 04:48:35 +0200

    added HTTPS to APIs

--- src/apis/APIData.js
+++ src/apis/APIData.js
@@ -2,19 +2,19 @@
 
 
 export const GetAutoCompleteSearchText=(text)=>{
-  return "http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="+apikey+"&q="+text
+  return "https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey="+apikey+"&q="+text
 }
 
 export const GetCurrentWeather=(locationKey)=>{
-  return "http://dataservice.accuweather.com/currentconditions/v1/"+locationKey+"?apikey="+apikey
+  return "https://dataservice.accuweather.com/currentconditions/v1/"+locationKey+"?apikey="+apikey
 }
 export const GetDailyForecastlocationKey=(locationKey)=>{
-  return "http://dataservice.accuweather.com/forecasts/v1/daily/5day/"+locationKey+"?apikey="+apikey
+  return "https://dataservice.accuweather.com/forecasts/v1/daily/5day/"+locationKey+"?apikey="+apikey
 }
 
 export const GetGeoLocation=(lat,long)=>{
    
-    return "http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey="+apikey+"&q="+lat+"%2C"+long
+    return "https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey="+apikey+"&q="+lat+"%2C"+long
 }
 
 export const GetDefaultLocation=()=>{
